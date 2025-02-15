7392e6ad6d5e73e75fa2a307c4bed972218b4aeb
nbrader <nbrader+github@gmail.com>
2025-02-15 20:28:15 +0000

    I'm debugging why the @ isn't appearing. It appears in the raw print version...

--- day15 - Warehouse Woes/day15-warehouse-woes/app/Main.hs
+++ day15 - Warehouse Woes/day15-warehouse-woes/app/Main.hs
@@ -23,48 +23,68 @@
 module Main (main) where
 
 import Data.Maybe
+import Data.Tuple
+import Data.Function
+import Data.Bifunctor
 import Data.List.Split
+import qualified Data.Map as M
 
 import AsciiWorld
 import WalkableWorld
 
-data KeyType = Original | Part Int deriving (Show, Eq, Ord)
-keyTypeToMaybe Original   = Nothing
-keyTypeToMaybe (Part x) = Just x
-maybeToKeyType Nothing  = Original
-maybeToKeyType (Just x) = Part x
-isOriginal = isNothing . keyTypeToMaybe
-isPart = isJust . keyTypeToMaybe
-getPartNum = fromJust . keyTypeToMaybe
+data MaskType  = Box | Wall | InternalMaskType
+    deriving (Show, Eq, Ord)
 
-data Key k = Key {keyType :: KeyType, keyValue :: k} deriving (Show, Eq, Ord)
+data PointsType = Robot | InternalPointsType
+    deriving (Show, Eq, Ord)
 
 main :: IO ()
 main = do
-    contents <- readFile "input/day15 (data).csv"
+    contents <- readFile "input/day15 (example).csv"
     
     let [worldStr,instructionsStr] = map unlines . splitOn [[]] . lines $ contents
         
-        initWorld :: WalkableWorld (Key Char) (Key Char)
-        initWorld = readWorld (Just . WKMask . Key Original) worldStr
-        height = wwHeight initWorld
-        asciiWorld = wwRawAsciiWorld initWorld
-    
-    let
+        charAssoc :: [(Char, WorldKey MaskType PointsType)]
+        charAssoc =
+            [   ('@', WKPoints Robot),
+                ('O', WKMask Box),
+                ('#', WKMask Wall)
+            ]
+        
+        fromCharMap = M.fromList charAssoc
+        toCharMap = M.fromList (map swap charAssoc)
+        
+        fromChar :: Char -> Maybe (WorldKey MaskType PointsType)
+        fromChar = flip M.lookup fromCharMap
+        
+        toChar :: WorldKey MaskType PointsType -> Char
+        toChar = fromMaybe 'I' . flip M.lookup toCharMap
+        
+        initWorld :: WalkableWorld MaskType PointsType
+        initWorld = readWorld fromChar worldStr
+        
         bgChar :: Char
         bgChar = '.'
         
-        maskToChar :: (Key Char) -> Char
-        maskToChar (Key Original c) = c
-        maskToChar (Key (Part n) c) = c
-        
-        pointsToChar :: (Key Char) -> Char
-        pointsToChar (Key Original c) = c
-        pointsToChar (Key (Part n) c) = c
-        
-        nameZOrder :: WorldKey (Key Char) (Key Char) -> WorldKey (Key Char) (Key Char) -> Ordering
+        nameZOrder :: WorldKey MaskType PointsType -> WorldKey MaskType PointsType -> Ordering
         nameZOrder = compare
         
-     in printWorld bgChar maskToChar pointsToChar nameZOrder initWorld
+        nameZOrder' :: WorldKey (Ext_Int MaskType WWMaskKey) (Ext_Int PointsType WWPointsKey) -> WorldKey (Ext_Int MaskType WWMaskKey) (Ext_Int PointsType WWPointsKey) -> Ordering
+        nameZOrder' = nameZOrder `on` conversion
+        
+        conversion :: WorldKey (Ext_Int MaskType WWMaskKey) (Ext_Int PointsType WWPointsKey) -> WorldKey MaskType PointsType
+        conversion = toWorldKey . bimap fromExternal1 fromExternal2 . fromWorldKey
+    
+    print initWorld
+    printWorld bgChar (toChar . WKMask) (toChar . WKPoints) nameZOrder initWorld
+    printRawAsciiWorld bgChar (toChar . WKMask . fromExternal1) (toChar . WKPoints . fromExternal2) nameZOrder' initWorld
     
     putStrLn . concat . lines $ instructionsStr
+
+fromExternal1 :: Ext_Int MaskType WWMaskKey -> MaskType
+fromExternal1 (External x) = x
+fromExternal1 (Internal y) = InternalMaskType
+
+fromExternal2 :: Ext_Int PointsType WWPointsKey -> PointsType
+fromExternal2 (External x) = x
+fromExternal2 (Internal y) = InternalPointsType
