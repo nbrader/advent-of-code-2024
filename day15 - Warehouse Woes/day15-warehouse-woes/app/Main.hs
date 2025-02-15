247fc9b29600415cefdbf1a205c195e53d8eb37d
nbrader <nbrader+github@gmail.com>
2025-02-15 17:03:21 +0000

    Removed ambiguity caused by the outer project declaring the inner library. At least I think that's what was going on. I got an error about ambiguity of library when trying to run Main.hs until I changed this. I also gave Main.hs the usual shebang stuff so it gets run as a script. It seems to work.

--- day15 - Warehouse Woes/day15-warehouse-woes/app/Main.hs
+++ day15 - Warehouse Woes/day15-warehouse-woes/app/Main.hs
@@ -1,3 +1,25 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-21.22 ghci --package containers-0.6.7 --package split-0.2.3.5 --package safe-0.3.19 --package QuickCheck-2.14.3 --package ansi-terminal-0.11.5 --package linear --package array
+
+----------------------------------
+----------------------------------
+----  Day 15: Warehouse Woes  ----
+----------------------------------
+----------------------------------
+{-
+    To build, run the following shell command in this directory:
+        stack --resolver lts-21.22 ghc --package containers-0.6.7 --package split-0.2.3.5 --package safe-0.3.19 --package QuickCheck-2.14.3 --package ansi-terminal-0.11.5 --package linear --package array -- '.\day15.hs' -O2
+-}
+
+------------
+-- Output --
+------------
+-- *Main> day15part1
+-- 
+
+-- *Main> day15part2
+-- 
+
 module Main (main) where
 
 import AsciiWorld (AsciiWorld(), emptyAsciiWorld)
